26f74d2f4d22d30e40a9cdfbd460f9c82cfc9e74
Tanukichi-35 <yo-fu@outlook.jp>
2024-06-04 09:53:30 +0900

    first commit

---
+++ src/resources/js/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Share from './share.jsx'
+import '../css/sanitize.css'
+import '../css/app.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Share />
+  </React.StrictMode>,
+)
