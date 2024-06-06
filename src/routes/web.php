a496d57362c4c1cf4e3e195e9cdd3d39c3481ce9
Tanukichi-35 <yo-fu@outlook.jp>
2024-06-06 12:59:46 +0900

    404Error時にホームにリダイレクトするよう変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+// ページが見つからない場合はトップページにリダイレクト
+Route::fallback(static fn () => redirect()->to('/'));
